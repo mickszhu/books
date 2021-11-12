b88a1db26c76ec1cfbd225d4594a761e78d21087
mickszhu <18565729157@163.com>
2021-11-12 15:47:44 +0800

    更新说明

--- src/test/java/com/mickszhu/demo/books/BaseTest.java
+++ src/test/java/com/mickszhu/demo/books/BaseTest.java
@@ -3,10 +3,16 @@ package com.mickszhu.demo.books;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        locations = {
+                "classpath:/mybatis/*.xml",
+        }
+)
 public class BaseTest {
 
     @Test
