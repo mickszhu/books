278cd6ea83853bc2f22cfab6eaac8465647b280b
mickszhu <18565729157@163.com>
2021-11-12 22:50:04 +0800

    更新说明

--- src/test/java/com/mickszhu/demo/books/test/SortTest.java
+++ src/test/java/com/mickszhu/demo/books/test/SortTest.java
@@ -1,5 +1,6 @@
 package com.mickszhu.demo.books.test;
 
+import com.jayway.jsonpath.internal.function.numeric.Min;
 import com.mickszhu.demo.books.BaseTest;
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,6 +9,10 @@ import org.junit.Test;
 @Slf4j
 public class SortTest  extends BaseTest {
 
+    /**
+     * 每次遍历的时候，将前面找出的最小值，看成一个有序的列表，
+     * 后面的看成无序的列表，然后每次遍历无序列表找出最小值。
+     */
     @Test
     public void selectSort(){
         int arr[] = {6, 5, 3, 2, 4};
@@ -34,5 +39,53 @@ public class SortTest  extends BaseTest {
         for (int i : arr) {
             log.info("输出：{}",i);
         }
+    }
+
+    /**
+     * 默认将第一数据看成有序列表，后面无序的列表循环每一个数据，如果比前面的数据小则插入（交换）。否则退出
+     */
+    @Test
+    public  void insertSort() {
+
+        int arr[] = {7, 5, 3, 2, 4};
+
+        //插入排序
+        for (int i = 1; i < arr.length; i++) {
+            //外层循环，从第二个开始比较
+            for (int j = i; j > 0; j--) {
+                //内存循环，与前面排好序的数据比较，如果后面的数据小于前面的则交换
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                } else {
+                    //如果不小于，说明插入完毕，退出内层循环
+                    break;
+                }
+            }
+        }
+        for (int i : arr) {
+            log.info("输出：{}",i);
+        }
+    }
+
+    /**
+     * 股票的最大利润
+     * 假设把某股票的价格按照时间先后顺序存储在数组中，请问买卖该股票一次可能获得的最大利润是多少？
+     */
+    @Test
+    public void maxProfitTest(){
+        int prices[] = {4, 2, 4, 9, 1,7,5,6};
+        int res = 0, min = prices[0];
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] <= min) {
+                min = prices[i];
+            }else {
+                res = Math.max(res, prices[i] - min);
+            }
+        }
+
+        log.info("打印结果：{}",res);
+        //log.info("max:{},maxIndex:{},mix:{},mixIndex:{},diffren;{}",max,maxIndex,mix,mixIndex,diffren);
     }
 }
